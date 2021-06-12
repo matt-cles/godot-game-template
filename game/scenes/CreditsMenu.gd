8f6378908a7f474ff5f7e4fbf8928de832b54b85
Matt Cles <matthew@cles.dev>
2021-06-12 17:16:43 +0800

    update credits menu

--- game/scenes/CreditsMenu.gd
+++ game/scenes/CreditsMenu.gd
@@ -2,3 +2,7 @@ extends Submenu
 
 func _on_Back_pressed():
 	return_focus()
+
+# Open the link to the template repo on link click
+func _on_TemplateShoutOut_meta_clicked(_meta):
+	var _err = OS.shell_open("https://github.com/matt-cles/godot-game-template")
